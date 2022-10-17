dd9e628f68d052b42f6d761be5ff0fdb21e7cd03
Franco Pelúa <erekkles@gmail.com>
2022-10-17 20:39:10 -0300

    Costs table are shown and updated based on user input

--- js/cart.js
+++ js/cart.js
@@ -3,13 +3,29 @@ initialRender();
 const CART_PRODUCTS = await getJSONData(CART_INFO_URL + '25801' + EXT_TYPE);
 
 const PRODUCTS_CONTAINER_DIV = $('#products-container'); 
-let { articles } = CART_PRODUCTS.data;
+const OPTIONS_FORM = $('#optionsForm');
 const USER_ADDED_ARTICLES = JSON.parse(localStorage.getItem('cartItems'));
+const SUBTOTAL_P = $('#subtotalPrice');
+const DELIVERY_COST_P = $('#deliveryCost');
+const TOTAL_COST_P = $('#totalCost');
+let deliveryCost;
+let subtotal;
+let totalCost;
+
+let { articles: articles_information } = CART_PRODUCTS.data;
+
 
 if(USER_ADDED_ARTICLES) articles = [...articles, ...USER_ADDED_ARTICLES];
 
+let articles = articles_information.map(function(article) {
+  return {
+    ...article, 
+    totalCost: article.count * article.unitCost
+  }
+})
+
 articles.forEach((article) => {
-    const { id, name, count, unitCost, currency, image } = article;
+    const { id, name, count, unitCost, currency, image, totalCost } = article;
 
     let htmlToAdd = `
         <tr>
@@ -23,10 +39,10 @@ articles.forEach((article) => {
               ${currency} ${unitCost}
             </td>
             <td>
-              <input value="${count}" type="number" class="form-control" id="${id}", data-price="${unitCost}"/>
+              <input value="${count}" type="number" class="form-control" id="${id}" />
             </td>
             <td>
-              <b data-priceOf="${id}">${currency} ${(unitCost * count)}</b>
+              <b data-priceOf="${id}">${currency} ${totalCost}</b>
             </td>
         </tr>
     `
@@ -34,20 +50,77 @@ articles.forEach((article) => {
     PRODUCTS_CONTAINER_DIV.insertAdjacentHTML('beforeEnd', htmlToAdd)
 });
 
-function changeSubTotalPrice(e) {
+function changeProductPrice(e) {
   e.stopPropagation()
 
   const { id: PRODUCT_ID, value: NEW_AMOUNT } = e.target;
 
   if(!NEW_AMOUNT || NEW_AMOUNT <= 0) return e.target.value = 1;
 
-  const PRODUCT_PRICE = Number(e.target.getAttribute('data-price'));
+  const PRODUCT_INDEX = articles.findIndex(article => article.id == PRODUCT_ID);
 
-  const PRICE_CONTAINER = $(`b[data-priceOf='${PRODUCT_ID}']`);
+  let product = articles[PRODUCT_INDEX];
+
+  const { unitCost: PRODUCT_PRICE } = product;
 
   const NEW_PRICE = PRODUCT_PRICE * Number(NEW_AMOUNT);
 
-  PRICE_CONTAINER.textContent = `USD ${NEW_PRICE}`;
+  product.count = Number(NEW_AMOUNT);
+  product.totalCost = NEW_PRICE;
+
+  renderNewProductPrice(NEW_PRICE, PRODUCT_ID)
 }
 
-PRODUCTS_CONTAINER_DIV.addEventListener('change', (e) => changeSubTotalPrice(e))
+function renderNewProductPrice(new_price, prod_id) {
+  const PRICE_CONTAINER = $(`b[data-priceOf='${prod_id}']`);
+  
+  PRICE_CONTAINER.textContent = `USD ${new_price}`;
+}
+
+function renderSubTotalAmount() {
+  subtotal = articles.map(article => article.totalCost).reduce((prevCost, currCost) => prev + curr);
+  SUBTOTAL_P.textContent = `USD ${subtotal}`
+}
+
+function renderDeliveryCost() {
+  const SUBTOTAL = articles.map(article => article.totalCost).reduce((prevCost, currCost) => prev + curr);
+  const CHECKED_RADIO_VALUE = Array.from(document.querySelectorAll('input[type="radio"][name="delivery"]')).find(radio => radio.checked).id;
+
+  switch(CHECKED_RADIO_VALUE) {
+    case "premium":
+      deliveryCost = (15 * SUBTOTAL) / 100;
+      break;
+    case "express":
+      deliveryCost = (7 * SUBTOTAL) / 100;
+      break;
+    case "standard":
+      deliveryCost = (5 * SUBTOTAL) / 100;
+      break;
+  }
+
+  DELIVERY_COST_P.textContent = `USD ${deliveryCost}`
+}
+
+function renderTotalCost() {
+  totalCost = deliveryCost + subtotal;
+  TOTAL_COST_P.textContent = `USD ${totalCost}`
+} 
+
+
+PRODUCTS_CONTAINER_DIV.addEventListener('change', function(e) {
+  changeProductPrice(e);
+  renderSubTotalAmount();
+  renderDeliveryCost();
+  renderTotalCost();
+})  
+
+OPTIONS_FORM.addEventListener('click', function() {
+  renderSubTotalAmount();
+  renderDeliveryCost();
+  renderTotalCost();
+})
+
+// Initial render
+renderSubTotalAmount();
+renderDeliveryCost();
+renderTotalCost();
